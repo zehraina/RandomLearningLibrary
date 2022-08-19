d4910dd75a4685b754aba5837a844110f88bafe2
Ina <101892341+zehraina@users.noreply.github.com>
2022-08-19 16:23:47 +0530

    Update Deleting an element from an array.cpp

--- Deleting an element from an array.cpp
+++ Deleting an element from an array.cpp
@@ -1,48 +1,45 @@
 
  #include<iostream>
  using namespace std;
+
  int search(int arr[],int n,int s ){
     for(int i=0;i<s;i++){
-        
             if (arr[i]==n)
-                return i; 
-            
-            
+                return i;
         }
         return -1;
-
     }
-
  
- int main(){
-    int s;
- int arr[100], n;
+int main(){
+int arr[100], n, s, index;;
+ 
 cout<<"Enter the size of the array"<<endl;
 cin>>s;
+ 
 cout<<"Enter the elements to populate the array"<<endl;
-
 for (int i=0;i<s;i++){
     cin>>arr[i];
 }
+ 
 cout<<"Enter the element you wish to search for"<<endl;
 cin>>n;
-int index;
-if (search(arr,n,s)!=-1){
+
+if (search(arr, n, s)!=-1){
     cout<<"Deleting the found element..."<<endl;
-    index=search(arr,n,s);
+    index=search(arr, n, s);
    
-{for(int i=index;i<s-1;i++){
+{for(int i=index; i<s-1; i++){
     arr[i]=arr[i+1];
-}}
+}
+}
+ 
 s=s-1;
 cout<<"Printing the array..."<<endl;
-for (int i=0;i<s;i++){
+for (int i=0; i<s; i++){
     cout<<arr[i]<<endl;
 }
 }
+ 
 else cout<<"Element not found.";
-
-
-
         return 0;
  }
